65ab599793d96c19820799ec0417ecee073b065d
Ruchika Madhubhashitha <madubashitharuchika@yahoo.com>
2018-02-18 22:38:16 +0530

    changes

---
+++ HospitalManagement/Models/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.Modules.Staffmanagement.Models
+{
+    public class Users1
+    {
+        public class Movie
+        {
+            public int ID { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+            public string Position { get; set; }
+
+
+        }
+    }
+}
--- HospitalManagement/Program.cs
+++ HospitalManagement/Program.cs
@@ -14,6 +14,7 @@ namespace HospitalManagement
     {
         public static void Main(string[] args)
         {
+
             BuildWebHost(args).Run();
         }
 
