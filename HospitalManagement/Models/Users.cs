8f8bd9f6f33be6ef8f549b242262645f4c6a7c0e
Ruchika Madhubhashitha <madubashitharuchika@yahoo.com>
2018-02-20 22:38:15 +0530

    commit..abcd

--- HospitalManagement/Models/Users.cs
+++ HospitalManagement/Models/Users.cs
@@ -7,14 +7,16 @@ namespace HospitalManagement.Modules.Staffmanagement.Models
 {
     public class Users1
     {
-        public class Movie
-        {
+       
+        
             public int ID { get; set; }
             public string UserName { get; set; }
             public string Password { get; set; }
             public string Position { get; set; }
+        public string abc { get; set; }
 
 
-        }
+
+
     }
 }
