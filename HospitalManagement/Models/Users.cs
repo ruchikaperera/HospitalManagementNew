1b29e0d6859d7818d3e7fb35c17bf8ae0e950ee3
Ruchika Madhubhashitha <madubashitharuchika@yahoo.com>
2018-02-20 23:36:02 +0530

    hiii

--- HospitalManagement/Models/Users.cs
+++ HospitalManagement/Models/Users.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace HospitalManagement.Modules.Staffmanagement.Models
 {
-    public class Users1
+    public class Users
     {
        
         
@@ -13,7 +13,7 @@ namespace HospitalManagement.Modules.Staffmanagement.Models
             public string UserName { get; set; }
             public string Password { get; set; }
             public string Position { get; set; }
-        public string abc { get; set; }
+        public string ruchika { get; set; }
 
 
 
